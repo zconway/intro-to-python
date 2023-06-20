5cb01f32c5422739fe4ee7bc7ad89ceeb9e6a4de
Replit user <>
2023-06-20 11:08:33 +0000

    completed ex 4

--- session_02/exercises_2.py
+++ session_02/exercises_2.py
@@ -3,59 +3,77 @@
 ## Section A
 # 1. Create two variables that hold the width and height of a rectangle, work out and store the area in a third variable. 
 # Print out the string: `Rectangle of width <x> and height <y> has an area of <area>`.
-
+# width = 4
+# length = 6
+# area = width * length
+# print(area)
 
 
 # 2. Write code that prints the length of the string, 'python'.
-
+# print(len('python'))
 
 
 # 3. Print out the first and third letter of the word 'python'.
-
+# word = "python"
+# print(word[0] + word[2])
 
 
 # 4. Ask the user to enter their name, and print out `Hello, <name>`.
-
-
+# name = input("What is your name? ")
+# print("Hello, " + name)
 
 # 5. Ask the user to enter their age, tell them how old they will be in 15 years time.
-
+# age = int(input("How old are you? "))
+# print("In 15 years you will be " + str(age+15) + " years old!")
 
 
 # 6. Combine the two input statements above and print out the message `Hello, <name>, you are currently <age> years old. 
 #   In 15 years time you will be <age_in_15_years_time>`.
-
+# print("Hello, " + name + ", In 15 years you will be " + str(age+15) + " years old!")
 
 
 # 7. Ask the user to enter their hometown, print it out in uppercase letters.
-
-
+# town = input("What is your hometown? ")
+# print(town.upper())
 
 # 8. Ask the user to enter their favourite colour and find out the length of the colour they input.
-
+# colour = input("What is your favourite colour?")
+# print(len(colour))
 
 
 # 9. Ask the user to enter the weather and the month. Print out the string, `It is <month> and it is <weather> today`.
-
-
+# month = input("What month is it? ")
+# weather = input("What is the weather like today? ")
+# print("It is " + month + " and it is " + weather + " today")
 
 # 10. Ask the user to enter 5 different temperatures and the month. Work out the average temperature and print out the string: 
 #   `It is <month> and the average temperature is <average_temperature> degrees celsius`.
-
+# month = input("What month is it? ")
+# temp1 = int(input("What is the temperature on day 1? "))
+# temp2 = int(input("What is the temperature on day 2? "))
+# temp3 = int(input("What is the temperature on day 3? "))
+# temp4 = int(input("What is the temperature on day 4? "))
+# temp5 = int(input("What is the temperature on day 5? "))
+# avg_temp = (temp1 + temp2 + temp3 + temp4 + temp5) / 5
+# print("It is " + month + " and the average temperature is " + str(avg_temp) + " degrees celsius")
 
 
 # 11. Print out the above sentence but make the month upper case.
-
+# print("It is " + month.upper() + " and the average temperature is " + str(avg_temp) + " degrees celsius")
 
 
 # 12. Create a variable that holds your favourite animals and print it out. 
 #    Make sure the animals are all on different lines and tabbed.
-
-
+# fav_animals = "\tDog\n\tCat\n\tElephant"
+# print(fav_animals)
 
 # 13. Ask the user to enter their name as well as a number. 
 #    Print out the uppercase character at that position in the string.
-
+# name = input("What is your name? ")
+# number = int(input("enter a number: "))
+# print(name.upper()[number])
 
 
 # 14. Slice the string with steps of 2, excluding the first and last characters of the string "WelcometoPython".
+string = "WelcometoPython"
+print(string[1:-1:2])
--- session_04/exercises_4.py
+++ session_04/exercises_4.py
@@ -2,31 +2,31 @@
 
 ## Section A
 # 1. Create the following list of items: Apples, Cherries, Pears, Pineapple, Peaches, Mango. Then print the list.
-
-
+items = ["Apples", "Cherries", "Pears", "Pineapple", "Peaches", "Mango"]
+print(items)
 
 # 2. Add "Grapes" to the list and print the list.
-
-
+items.append("Grapes")
+print(items)
 
 # 3. Change "Pears" to "Strawberries" and print the list.
-
-
+items[2] = "Strawberries"
+print(items)
 
 # 4. Remove "Apples" from the list and print the list.
-
-
+del(items[0])
+print(items)
 
 # 5. Print out the current length of the list.
-
+print(len(items))
 
 
 # 6. Order the list alphabetically.
-
+items.sort()
 
 
 # 7. Print out the list again.
-
+print(items)
 
 
 
@@ -34,40 +34,67 @@
 
 ## Section B
 # 1. Loop through the list you created in section A and print each item out.
-
+for item in items:
+  print(item)
 
 
 # 2. Print the numbers 1 to 100 (including the number 100).
-
+for num in range(1,101):
+  print(num)
 
 
 # 3. Print all odd numbers from 1 to 100.
-
+for num in range(1,101):
+  if num%2 == 1:
+    print(num)
 
 
 # 4. The modern olympics started in 1896, print the years they have been held (bonus points to skip the years it has not been held 1916, 1940, 1944, 2020).
-
+for year in range(1896, 2024, 4):
+  if year != 1916 and year != 1940 and year != 1944 and year != 2020:
+    print(year)
 
 
 # 5. Create a list of ten random numbers. Loop through your list and count the number of even numbers and the number of odd numbers.
-
-
-
+import random
+randomList = []
+for i in range(0,10):
+  n = random.randint(1,30)
+  randomList.append(n)
+print(randomList)
+odd = 0
+even = 0
+for i in randomList:
+  if i%2 == 0:
+    even = even+1
+  else:
+    odd = odd + 1
+print("Even: "+ str(even))
+print("Odd: "+ str(odd))
 # 6. Create a list of five names. Write a loop that will print "Hello" plus each name in the list.
-
-
+names = ["Zac", "Bob", "John", "Meg", "Alex"]
+for name in names:
+  print("Hello " + name)
 
 # 7. Create a loop to go through each letter of the word "supercalifragilisticexpialidocious".
-
-
+word = "supercalifragilisticexpialidocious"
+for i in word:
+  print(i)
 
 # 8. Create a list of 5 numbers. Write a for loop which appends the square of each number to the new list.
-
+nums = [1,2,3,4,5]
+nums2 = []
+for i in nums:
+  nums2.append(i**2)
+print(nums2)
 
 
 # 9. Create a list with five names in. Write a for loop which appends  Dr. to each name in the new list.
-
-
+names = ["Zac", "Bob", "John", "Meg", "Alex"]
+names2 = []
+for name in names:
+  names2.append("Dr. " + name)
+print(names2)
 
 # 10. FizzBuzz – Write a program that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for multiples of five, print "Buzz". 
 #    For numbers which are multiples of both three and five, print "FizzBuzz".
@@ -89,3 +116,13 @@
 #     14
 #     FizzBuzz
 #     ```
+for num in range(1,101):
+  if num%3 == 0 and num%5 == 0:
+    print("FizzBuzz")
+  elif num%3 == 0:
+    print("Fizz")
+  elif num%5 == 0:
+    print("Buzz")
+  else:
+    print(num)
+
