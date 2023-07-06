3db2e60f9c6ebc35ae9272e81230ceba3b2a1688
Replit user <>
2023-07-06 12:56:59 +0000

    ses 5 + 6

--- session_05/exercises_5.py
+++ session_05/exercises_5.py
@@ -2,42 +2,80 @@
 
 ## Section A
 # 1. Print 10 random numbers.
+import random
+# randomList = []
+# for i in range(0,10):
+#   print(random.randint(0,10000))
 
 
 
 # 2. Keep asking the user to enter a number until they enter the number 7, then print "Wow lucky number 7!".
 #     - Rewrite so that the number being guessed is a random value from 1 to 10 instead of number 7 .
-
-
+import random
+# randNum = random.randint(1,11)
+# guessed = False
+# while guessed == False:
+#   guess = int(input("Guess the number between 1-10: "))
+#   if guess == randNum:
+#     guessed = True
+#     print("Wow lucky number "+str(randNum)+"!")
 
 # 3. The area of a rectangle is width multiplied by height. Ask the user to enter a width and height in cm, then print the area to the whole square metre. 
 #   E.g. 240cm x 80cm = 19200cm2 = 2m2.
-
-
+from math import floor
+# width = int(input("Enter height in cm: "))
+# height = int(input("Enter height in cm: "))
+# area = floor(width/10*height/10)
+# print("Area is "+str(area)+"m squared.")
 
 # 4. Ask the user for a password, if they enter the password "qwerty123", print "You have successfully logged in". 
 #   If they get it wrong, print "Password failure" and then ask them to enter it again. 
 #   Only allow them to enter the password wrong 3 times before printing "System Locked!".
-
-
+# count = 0
+# password = "qwerty123"
+# while True:
+#   count += 1
+#   e = input("Enter Password: ")
+#   if e == password:
+#     print("Correct!")
+#     break
+#   else:
+#     print("Password failure")
+#     if count >= 3:
+#       print("System Locked!")
+#       break
 
 # 5. Add up all the numbers from 1 to 500 and print the answer.
-
-
+# total = 0
+# count = 0
+# while count <= 500:
+#   total += count
+#   count += 1
+# print(total)
 
 # 6. Create a blank list. Ask the user to input 10 numbers (one should be the number 99) into the list. 
 #   Find the index at which the user entered the number 99.
-
-
+# list = []
+# for i in range(10):
+#   list.append(int(input("Enter number: ")))
+# print(list)
+# for i in range(10):
+#   if list[i] == 99:
+#     print("Index is "+str(i))
+#     break
 
 # 7. Print a multiplication table for the number 18 up to 15. E.g.
 #     1 x 18 = 18
 #     2 x 18 = 36
-
+# for i in range(16):
+#   print(str(18*i))
 
 
 # 8. Print the numbers 1 to 100 (including the number 100) using a while loop.
-
+# count = 1
+# while count < 101:
+#   print(count)
+#   count += 1
 
 
 # 9. Rewrite question B8 from session 3 using a while loop
@@ -49,14 +87,62 @@
 #         e.	25 to 45 – E
 #         f.	Below 25 - F
 #     Ask user to enter the lesson and the marks for three lessons and print out the corresponding grades for the lesson.
+# lesson = input("Enter lesson: ")
+# grade1 = int(input("Enter grade 1: "))
+# grade2 = int(input("Enter grade 2: "))
+# grade3 = int(input("Enter grade 3: "))
 
+# finalGrade = (grade1+grade2+grade3)/3
+
+# if finalGrade > 80:
+#   print("A")
+# elif finalGrade >= 60:
+#   print("B")
+# elif finalGrade >= 50:
+#   print("C")
+# elif finalGrade >= 45:
+#   print("D")
+# elif finalGrade >= 25:
+#   print("E")
+# else:
+#   print("F")
 
 
 # 10. Ask the user to enter the names of people who entered a prize draw. Select a random winner and print their name
+# names = []
 
+# for i in range(3):
+#   names.append(input("Enter name: "))
 
+# num = random.randint(0,2)
+# print("The winner is: "+names[num])
 
 # 11. Create a rock, paper, scissors game which is run against computer. 
 #    This is a game where you select either rock/paper/scissors and you compare it to your opponents choice. 
 #    Rock beats scissors, paper beats rock, scissors beats paper. If both choose the same, then you play again.
 #       + Expand this so that its best of 3 games
+type = ["Rock", "Paper", "Scissor"]
+
+while True:
+  comp = type[random.randint(0,2)]
+  choice = input("Rock, Paper, Scissors: ")
+  print("The computer chose "+comp)
+  if choice == comp:
+    print("It's a tie!")
+  elif choice == "Rock":
+    if comp == "Paper":
+      print("You lose!")
+    else:
+      print("You win!")
+  elif choice == "Paper":
+    if comp == "Scissors":
+      print("You lose!")
+    else:
+      print("You win!")
+  elif choice == "Scissors":
+    if comp == "Rock":
+      print("You lose!")
+    else:
+      print("You win!")
+  else:
+    print("Error!")
--- session_06/exercises_6.py
+++ session_06/exercises_6.py
@@ -2,36 +2,84 @@
 
 ## Section A
 # 1. Create the following dictionary for an apple: Type = "Bramley", Price = 0.39, Colour = "Green".
-
+apple = {
+  "type": "Bramley",
+  "price": 0.39,
+  "colour": "Green"
+}
 
 
 # 2. Add the best before date to the dictionary - print the dictionary.
-
-
+apple["bestBefore"] = "10/07/2023"
+# print(apple)
 
 # 3. Change the price to 0.41 - print the dictionary.
-
-
+apple["price"] = 0.41
+# print(apple)
 
 # 4. Set the apple to be on offer using a Boolean - print the dictionary.
-
-
+apple["onOffer"] = True
+# print(apple)
 
 # 5. The offer has now expired, remove the key/value from the dictionary - print the dictionary.
-
+del(apple["onOffer"])
+print(apple)
 
 # <---------------------------------------------------------------------------------------------->
 
 ## Section B
 # 1. Ask the user to enter a persons name, if they enter a name, ask for the persons age. Store this information in a dictionary inside a list. 
 #   Continue to ask for names until no name is given. Then print out all of the names and ages collected.
+# list = []
+# name = None
+# while name != "":
+#   name = input("Enter name: ")
+#   age = input("Enter age: ")
+#   if name and age:
+#     list.append({
+#       "name": name,
+#       "age": int(age)
+#     })
 
-
+# print(list)
 
 # 2. Create a restaurants menu with 5 items. Store this information in a dictionary inside a list. 
 #   Each item in the menu should have the name of the item, the price and if it's vegetarian friendly (make at least one vegetarian friendly dish). 
 #   Print out the entire menu. Print out the name of the vegetarian option(s).
+menu = [
+  {
+    "name": "Burger",
+    "price": 5.50,
+    "v": False
+  },
+  {
+    "name": "Chips",
+    "price": 3,
+    "v": True
+  },
+  {
+    "name": "Hot Dog",
+    "price": 6,
+    "v": False
+  },
+  {
+    "name": "Salad",
+    "price": 5,
+    "v": True
+  },
+  {
+    "name": "Chicken",
+    "price": 6,
+    "v": False
+  },
+]
 
+print(menu)
+print("")
+print("Vegetarian options:")
+for item in menu:
+  if item["v"] == True:
+    print(item)
 
 
 # 3. The beetle game is a dice game where depending on what you roll is how much of the beetle you can draw.
@@ -43,3 +91,78 @@
 #   If you roll a 1, you can draw the mouth (but remember, you cannot draw a mouth without a head)
 #   You need 6 legs, 2 antenna, 2 eyes, 1 mouth.
 #   The player to complete the beetle in the fewest rolls of the dice wins. Create the beetle game.
+
+import random
+randNum = random.randint(1,11)
+beetle = {
+  "legs": 0,
+  "antenna": 0,
+  "eyes": 0,
+  "mouth": False,
+  "body": False,
+  "head": False
+}
+rollCount = 0
+
+while beetle["legs"] < 6 or beetle["antenna"] < 2 or beetle["eyes"] < 2 or beetle["mouth"] == False:
+  print("")
+  rollCount += 1
+  input("Press enter to roll the dice for the "+str(rollCount)+" time!")
+  randNum = random.randint(1,6)
+  print("")
+  print("You rolled: "+str(randNum))
+  print("")
+  if randNum == 6:
+    if beetle["body"] == True:
+      print("The beetle already has a body!")
+    else:
+      beetle["body"] = True
+      print("Your beetle now has a body!")
+  if randNum == 5:
+    if beetle["head"] == True:
+      print("The beetle already has a head!")
+    else:
+      beetle["head"] = True
+      print("Your beetle now has a head!")
+  if randNum == 4:
+    if beetle["body"] == True:
+      if beetle["legs"] < 6:
+        beetle["legs"] += 1
+        print("Your beetle now has "+str(beetle["legs"])+" legs!")
+      else:
+        print("Your beetle already has all of it's legs!")  
+    else:
+      print("Your beetle needs a body before you can add legs!")
+  if randNum == 3:
+    if beetle["head"] == True:
+      if beetle["antenna"] < 2:
+        beetle["antenna"] += 1
+        print("Your beetle now has "+str(beetle["antenna"])+" antenna!")
+      else:
+        print("Your beetle already has both of it's antenna!")  
+    else:
+      print("Your beetle needs a head before you can add antenna!")
+  if randNum == 2:
+    if beetle["head"] == True:
+      if beetle["eyes"] < 2:
+        beetle["eyes"] += 1
+        print("Your beetle now has "+str(beetle["eyes"])+" eyes!")
+      else:
+        print("Your beetle already has both of it's eyes!")  
+    else:
+      print("Your beetle needs a head before you can add eyes!")
+  if randNum == 1:
+    if beetle["head"] == True:
+      if beetle["mouth"] == False:
+        beetle["mouth"] = True
+        print("Your beetle now has a mouth!")
+      else:
+        print("Your beetle already has a mouth!")  
+    else:
+      print("Your beetle needs a head before you can add a mouth!")
+  print("")
+  print(beetle)
+
+print("")
+print("You win!")
+print("Number of rolls: "+str(rollCount))
